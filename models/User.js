2ee108f8b0cb6db18fe48083eff14b341ab64746
Essence1987 <129245370+Essence1987@users.noreply.github.com>
2023-08-27 12:34:41 -0600

    created User Model, sending up file structure

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true, 
+        trim: true, // trim whitespace
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid e-mail address'],
+    },
+    thoughts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+});
+
+userSchema.vitual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
