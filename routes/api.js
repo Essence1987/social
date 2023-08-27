57245f6a89bd7e2772c803f9352532119a0737b2
Essence1987 <129245370+Essence1987@users.noreply.github.com>
2023-08-27 13:12:41 -0600

    Sending up more API routes

--- routes/api.js
+++ routes/api.js
@@ -12,7 +12,19 @@ router.get('/users', async (req, res) => {
     }
 });
 
-// Route to create a new user
+// GET a single user by its _id and populated thought and friend data
+router.get('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        .populate('thoughts')
+        .populate('friends');
+        res.json(user);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// POST to create a new user
 
 router.post('/users', async (req, res) => {
     try {
@@ -22,4 +34,44 @@ router.post('/users', async (req, res) => {
         res.status(500).json(error);
     }
 });
+
+// POST to create a new friend
+router.post('/users/:userId/friends/:friendId', async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(
+            req.params.userId,
+            { $addToSet: { friends: req.params.friendId } },
+            { new: true }
+        );
+        res.json(user);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// Put to update a user by its _id
+
+router.put('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(user);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// DELETE to remove user by its _id
+
+router.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        // remove user's associated thoughts
+        await Thought.deleteMany({ username: user.username });
+        res.json(user);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
 
