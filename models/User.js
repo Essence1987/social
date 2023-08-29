d9f6c86943801c5083f47e3b531cb246dd8fa2e4
Essence1987 <129245370+Essence1987@users.noreply.github.com>
2023-08-28 22:12:36 -0600

    fixed friend display

--- models/User.js
+++ models/User.js
@@ -1,34 +1,39 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        unique: true,
-        required: true, 
-        trim: true, // trim whitespace
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/.+@.+\..+/, 'Please enter a valid e-mail address'],
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Please enter a valid e-mail address'],
     },
     thoughts: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Thought'
-        }
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
     ],
     friends: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
-        }
-    ]
-});
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: { virtuals: true }, // Include virtuals when converting to JSON
+  }
+);
 
-userSchema.virtual('friendCount').get(function() {
-    return this.friends.length;
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
 });
 
 const User = mongoose.model('User', userSchema);
